5bc773e3dee4dff126662490506ccdf2df77553b
florianthiery <web@florian-thiery.de>
2019-09-26 13:39:28 +0200

    Update sparqlR.R

--- sparqlR.R
+++ sparqlR.R
@@ -1,30 +1,25 @@
 #install.packages("WikidataQueryServiceR")
 library(WikidataQueryServiceR)
 
-# R's versions and release dates:
-sparql_query <- 'SELECT ?siteLabel ?label ?site
-WHERE {
-   ?site wdt:P31/wdt:P279* wd:Q839954 ;
-      wdt:P279 ?inst .
-    ?inst rdfs:label ?label.
-    FILTER(LANG(?label) = "en").
-  ?site rdfs:label ?siteLabel.
-    FILTER(LANG(?siteLabel) = "en").
-}'
+# query Wikidata SPARQL endpoint
+# https://w.wiki/8uP
+sparql_query <- 'SELECT ?item ?itemLabel ?motive ?motiveLabel
+WHERE
+{
+  VALUES ?objects {wd:Q41971267}
+?item wdt:P31 ?objects.
+  ?item wdt:P180 ?motive .
+SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+} ORDER BY ?motiveLabel'
+
 df <- query_wikidata(sparql_query)
-
-typeof(df)
-
 df2 <- as.data.frame(df)
-
-typeof(df2)
-
-View(df2)
+#View(df2)
 
 library(ggplot2)
 
 ggplot()+
-  geom_bar(data = df2, aes(x = label, fill = label))+
-  labs(y = "A",
-       title = "B")+
+  geom_bar(data = df2, aes(x = motiveLabel, fill = motiveLabel))+
+  labs(y = "count",
+       title = "Archaeological site")+
   theme_dark()
